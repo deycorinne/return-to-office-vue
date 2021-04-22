2f27ece6dd12fd5b6d117cfc5ffe458cacbe5766
Corinne Dey <cjdey@pbs.org>
2021-04-22 15:18:10 -0400

    continue moving to v 3 ;

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Welcome/>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Welcome from './components/Welcome.vue'
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
 <style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="welcome" v-if="welcome">
+
+    {{ message }}
+
+    <li v-for="state in states">
+        <button v-on:click="selectState(state)">
+            {{ state.name }}
+        </button>
+    </li>
+
+  </div>
+
+
+  <div id="selected" v-if="!welcome">
+
+    {{ selected.name }}
+
+    <button v-on:click="reset">
+        Reset
+    </button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data() {
+    return {
+      message: 'Welcome!',
+      welcome: true,
+      selected: null,
+      states: [
+          { 
+            name: 'MD',
+            id: 'MD',
+            barrier_states: [
+                "MD", "DC", "VA"
+            ]
+          },
+          { 
+            name: 'DC',
+            id: 'DC',
+            barrier_states: [
+                "DC", "VA"
+            ]
+          },
+          { 
+            name: 'VA',
+            id: 'VA' ,
+            barrier_states: [
+                "VA"
+            ]
+          }
+      ]
+    }
+  },
+  methods: {
+    selectState(state) {
+      this.selected = state;
+      this.welcome = false;
+    },
+    reset(event) {
+        this.welcome = true;
+        this.selected = null;
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
