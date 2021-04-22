8dd2c3be98544493a5dfa5c3564064793fcb8e1d
Corinne Dey <cjdey@pbs.org>
2021-04-22 15:41:51 -0400

    a few styles

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,22 +1,35 @@
 <template>
   <div id="welcome" v-if="welcome">
 
-    {{ message }}
+    <p class="font-semibold text-3xl">
+      Welcome
+    </p>
 
-    <li v-for="state in states">
-        <button v-on:click="selectState(state)">
+    <p class="font-semibold text-xl">
+      Please select your state:
+    </p>
+
+    <li v-for="state in states" class="list-none">
+        <button v-on:click="selectState(state)" 
+                class="bg-white hover:bg-gray-100 text-gray-800 
+                      font-semibold py-2 px-4 border border-gray-400 
+                      rounded shadow" >
             {{ state.name }}
         </button>
     </li>
 
   </div>
 
-
   <div id="selected" v-if="!welcome">
 
+    <p class="font-semibold text-xl">
     {{ selected.name }}
+    </p>
 
-    <button v-on:click="reset">
+    <button v-on:click="reset" 
+            class="bg-white hover:bg-gray-100 text-gray-800 
+                  font-semibold py-2 px-4 border border-gray-400 
+                  rounded shadow" >
         Reset
     </button>
 
@@ -25,10 +38,8 @@
 
 <script>
 export default {
-  name: 'Welcome',
   data() {
     return {
-      message: 'Welcome!',
       welcome: true,
       selected: null,
       states: [
